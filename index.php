2c482722961ca52ec9eb1ad27463f205c5128ef4
Matheus Maciel <srmatheusmaciel@hotmail.com>
2020-07-04 11:40:16 -0300

    Criei a Classe Usuario

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 
 require_once("config.php");
 
+/* -- AULA DE DAO número 01
+
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios); */
 
+$matheus = new Usuario();
+
+$matheus->loadbyId(1);
+
+echo $matheus;
 
  ?>
